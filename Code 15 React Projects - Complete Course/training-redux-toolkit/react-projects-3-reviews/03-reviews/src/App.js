eaad425827d3b9ceb30dcc32356449dff8cb35ef
Víctor Barreto <victorbarretolins@gmail.com>
2022-07-12 19:46:31 -0300

    improved html and features a litte

--- Code 15 React Projects - Complete Course/training-redux-toolkit/react-projects-3-reviews/03-reviews/src/App.js
+++ Code 15 React Projects - Complete Course/training-redux-toolkit/react-projects-3-reviews/03-reviews/src/App.js
@@ -1,32 +1,14 @@
-import './App.scss';
-import { useDispatch ,useSelector} from 'react-redux'
+import "./App.scss";
+import ReviewComponent from "./components/ReviewComponent";
 
-import {
-  currentIndex,
-  nextReview,
-} from './features/reviewSlice'
-
-import {reviews} from './utils/data'
-import { useState } from 'react';
 
 function App() {
 
-  const index = useSelector(currentIndex)
-  const {image, name, job, text} = reviews[index]
-
-  
-  const dispatch = useDispatch()
 
   return (
-    <article className='review'>
-      <div className='img-container'>
-        <img src={image} alt={name} className='person-img'></img>
-      </div>
-      <div className='button-container'>
-        <button className='next-btn' onClick={() => {dispatch(nextReview())}}>Next
-        </button>
-      </div>
-    </article>
+    <main>
+      <ReviewComponent/>
+    </main>
   );
 }
 
