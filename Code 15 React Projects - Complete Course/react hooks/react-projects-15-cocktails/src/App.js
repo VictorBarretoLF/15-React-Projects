8b683385b1a7e1093e6f883c571e42906563388b
Víctor Barreto <victorbarretolins@gmail.com>
2022-07-19 21:24:25 -0300

    terminando CSS da navbar

---
+++ Code 15 React Projects - Complete Course/react hooks/react-projects-15-cocktails/src/App.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar/>}>
+        <Route index element={<Home />}></Route>
+        <Route path="cocktail/:id" element={<h1>A page</h1>}></Route>
+        <Route path="about" element={<h1>About</h1>}></Route>
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ Code 15 React Projects - Complete Course/react hooks/react-projects-15-cocktails/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import logo from "../utils/svg/logo.svg";
+import "./scss/navbar.styles.scss";
+
+const navLinks = [
+  {
+    linkTo: "/",
+    text: "home",
+  },
+  {
+    linkTo: "/about",
+    text: "about",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <nav className="navbar">
+        <div className="nav-center">
+          <Link to="/">
+            <img src={logo} alt="cocktail db logo" className="logo" />
+          </Link>
+          <ul className="nav-links">
+            {navLinks.map(({ linkTo, text }, index) => {
+              return (
+                <li key={index}>
+                  <Link to={linkTo}>{text}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navbar;
