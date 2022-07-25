7666daa1aa8dbc2655d22e0178c97f7439ff14f7
Víctor Barreto <victorbarretolins@gmail.com>
2022-07-25 11:36:53 -0300

    finished fetching the tours

--- Code 15 React Projects - Complete Course/React Projects/react-projects-2-tours/project-using-classes/src/App.js
+++ Code 15 React Projects - Complete Course/React Projects/react-projects-2-tours/project-using-classes/src/App.js
@@ -2,10 +2,60 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
-  URL = "https://course-api.com/react-tours-project";
+  constructor() {
+    super();
+    this.URL = "https://course-api.com/react-tours-project";
+    this.state = {
+      loading: true,
+      tours: [],
+    };
+  }
+
+  fetchTours = async () => {
+    this.setState({ loading: true });
+
+    try {
+      const response = await fetch(this.URL);
+      const data = await response.json();
+      this.setState({ loading: false });
+      this.setState({ tours: data });
+    } catch (err) {
+      this.setState({ loading: false });
+      console.log(err);
+    }
+  };
+
+  componentDidMount() {
+    this.fetchTours();
+  }
 
   render() {
-    return <h2 className="App">URL</h2>;
+    const { loading, tours } = this.state;
+
+    if (loading) {
+      return (
+        <main>
+          <div className="loading">
+            <h1>loading...</h1>
+          </div>
+        </main>
+      );
+    }
+
+    if (tours.length === 0) {
+      return (
+        <main>
+          <div className="title">
+            <h2>no tours left</h2>
+            <button className="btn" onClick={() => {}}>
+              refresh
+            </button>
+          </div>
+        </main>
+      );
+    }
+
+    return <main>DATA</main>;
   }
 }
 
