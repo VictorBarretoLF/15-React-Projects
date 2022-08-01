8155f86212ae1a054dd9c260ad628940cb751d62
Víctor Barreto <victorbarretolins@gmail.com>
2022-08-01 12:15:08 -0300

    atualizando os arquivos

--- Code 15 React Projects - Complete Course/react-projects-04-accordion/src/App.js
+++ Code 15 React Projects - Complete Course/react-projects-04-accordion/src/App.js
@@ -2,12 +2,6 @@ import React, { Component } from "react";
 import Question from "./components/Question";
 import questions from "./utils/data";
 
-// {
-//   id: 3,
-//   title: "What is BankID?",
-//   info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
-// },
-
 class App extends Component {
   constructor() {
     super();
