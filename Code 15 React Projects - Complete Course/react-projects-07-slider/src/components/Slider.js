39ab89563edf4722bbb065f5cc29e42e46ca98be
Víctor Barreto <victorbarretolins@gmail.com>
2022-08-03 12:24:55 -0300

    added a CSS logic to the app

--- Code 15 React Projects - Complete Course/react-projects-07-slider/src/components/Slider.js
+++ Code 15 React Projects - Complete Course/react-projects-07-slider/src/components/Slider.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import people from "../utils/data";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 class Slider extends Component {
   constructor() {
@@ -20,15 +22,29 @@ class Slider extends Component {
         </div>
         <div className="section-center">
           {this.state.people.map(({ id, image, name, title, quote }, index) => {
+            let position = "nextSlide";
+            if (index === this.state.index) position = "activeSlide";
+            if (
+              this.state.index === index - 1 ||
+              (index === 0 && index === this.state.people.length - 1)
+            )
+              position = "lastSlide";
             return (
-              <article key={id}>
+              <article className={position} key={id}>
                 <img src={image} alt={name} className="person-img" />
                 <h4>{name}</h4>
                 <p className="title">{title}</p>
                 <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
               </article>
             );
           })}
+          <button className="prev">
+            <FiChevronLeft />
+          </button>
+          <button className="next">
+            <FiChevronRight />
+          </button>
         </div>
       </section>
     );
