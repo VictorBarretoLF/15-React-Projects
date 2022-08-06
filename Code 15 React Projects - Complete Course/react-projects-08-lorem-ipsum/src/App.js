18330baae759e4c939d8cc136ca1f9e9fe1f133a
Víctor Barreto <victorbarretolins@gmail.com>
2022-08-06 16:26:05 -0300

    finished the lorem ipsum project

--- Code 15 React Projects - Complete Course/react-projects-08-lorem-ipsum/src/App.js
+++ Code 15 React Projects - Complete Course/react-projects-08-lorem-ipsum/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import data from "./utils/data/data";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      text: [],
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(this.state.count);
+
+    if (amount <= 0) amount = 1;
+    if (amount > 8) amount = 8;
+
+    this.setState({ text: data.slice(0, amount) });
+  };
+
+  onChangeHandler = (e) => {
+    const { value } = e.target;
+    if (value >= 0) this.setState({ count: value });
+  };
+
+  render() {
+    const { count } = this.state;
+    const { onChangeHandler, handleSubmit } = this;
+    return (
+      <section className="section-center">
+        <h1>tired of boring lorem ipsum?</h1>
+        <form className="lorem-form" onSubmit={handleSubmit}>
+          <label htmlFor="amount">paragraphs:</label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            max="40"
+            autoComplete="true"
+            onChange={onChangeHandler}
+            value={count}
+          />
+          <button className="btn">generate</button>
+        </form>
+        <article className="lorem-text">
+          {this.state.text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </section>
+    );
+  }
 }
 
 export default App;
