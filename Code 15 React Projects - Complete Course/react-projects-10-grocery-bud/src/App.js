4df03d24ae3f01819dbf999bfef55eb595e22796
Víctor Barreto <victorbarretolins@gmail.com>
2022-08-09 11:21:36 -0300

    added items

--- Code 15 React Projects - Complete Course/react-projects-10-grocery-bud/src/App.js
+++ Code 15 React Projects - Complete Course/react-projects-10-grocery-bud/src/App.js
@@ -1,10 +1,66 @@
 import React, { Component } from "react";
 import List from "./components/List";
-import Alert from "./Alert";
+import Alert from "./components/Alert";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      list: [],
+      isEditing: false,
+      editID: null,
+      alert: { show: false, msg: "", type: "" },
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, isEditing, list } = this.state;
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      this.setState({ list: [...list, newItem] }, () => {
+        console.log(this.state);
+      });
+      this.setState({ name: "" });
+    }
+  };
+
   render() {
-    return <h2>grocery bud setup</h2>;
+    const { alert, isEditing, name, list } = this.state;
+    return (
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={this.handleSubmit}>
+          {alert.show && <Alert />}
+          <h3>grocery bud</h3>
+          <div className="form-control">
+            <input
+              type="text"
+              className="grocery"
+              placeholder="e.g. eggs"
+              value={name}
+              onChange={(e) => {
+                this.setState({ name: e.target.value });
+              }}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "edit" : "submit"}
+            </button>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} />
+            <button className="clear-btn">clear it</button>
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
--- Code 15 React Projects - Complete Course/react-projects-10-grocery-bud/src/components/List.js
+++ Code 15 React Projects - Complete Course/react-projects-10-grocery-bud/src/components/List.js
@@ -3,7 +3,27 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 
 class List extends Component {
   render() {
-    return <h2>list component</h2>;
+    const { items } = this.props;
+
+    return (
+      <div className="grocery-list">
+        {items.map(({ id, title }) => {
+          return (
+            <article key={id} className="grocery-item">
+              <p className="title">{title}</p>
+              <div className="btn-container">
+                <button type="button" className="edit-btn">
+                  <FaEdit />
+                </button>
+                <button type="button" className="delete-btn">
+                  <FaTrash />
+                </button>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    );
   }
 }
 
