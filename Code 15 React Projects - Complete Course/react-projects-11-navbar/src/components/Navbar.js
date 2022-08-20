fa1f2b19fef84bad93686dab05dc62c913175086
Víctor Barreto <victorbarretolins@gmail.com>
2022-08-20 16:41:47 -0300

    Basic Structure

--- Code 15 React Projects - Complete Course/react-projects-11-navbar/src/components/Navbar.js
+++ Code 15 React Projects - Complete Course/react-projects-11-navbar/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "../utils/helper-objects/navLinks";
+import logo from "../utils/svg/logo.svg";
 
 class Navbar extends Component {
   render() {
@@ -8,10 +9,38 @@ class Navbar extends Component {
       <nav>
         <div className="nav-center">
           <div className="nav-header">
-            
+            <img src={logo} className="logo" alt="logo" />
+            <button className="nav-toggle">
+              <FaBars />
+            </button>
           </div>
-          <div className="links-container show-container"></div>
-          <ul className="social-icons"></ul>
+          <div className="links-container show-container">
+            <ul className="links">
+              <li>
+                <a href="#">home</a>
+                <a href="#">about</a>
+                <a href="#">contact</a>
+                <a href="#">products</a>
+              </li>
+            </ul>
+          </div>
+          <ul className="social-icons">
+            <li>
+              <a href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     );
