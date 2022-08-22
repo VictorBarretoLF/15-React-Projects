69b2ac82e917d7b43a63452847484dfb92aaa508
Víctor Barreto <victorbarretolins@gmail.com>
2022-08-22 17:59:05 -0300

    finished navbar toggle and dinamic links

--- Code 15 React Projects - Complete Course/react-projects-11-navbar/src/components/Navbar.js
+++ Code 15 React Projects - Complete Course/react-projects-11-navbar/src/components/Navbar.js
@@ -1,51 +1,64 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "../utils/helper-objects/navLinks";
 import logo from "../utils/svg/logo.svg";
 
 class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showLinks: true,
+    };
+    this.linksRef = createRef();
+    this.linksContainerRef = createRef();
+  }
+
+  toggleLinks = () => {
+    this.setState({ showLinks: !this.state.showLinks });
+  };
+
+  componentDidUpdate = () => {
+    // console.log(this.state)
+    const linksHeight = this.linksRef.current.getBoundingClientRect().height;
+    if (this.state.showLinks) {
+      this.linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      this.linksContainerRef.current.style.height = "0px";
+    }
+  };
+
   render() {
     return (
       <nav>
         <div className="nav-center">
           <div className="nav-header">
             <img src={logo} className="logo" alt="logo" />
-            <button className="nav-toggle">
+            <button className="nav-toggle" onClick={this.toggleLinks}>
               <FaBars />
             </button>
           </div>
-          <div className="links-container show-container">
-            <ul className="links">
-              <li>
-                <a href="#a">home</a>
-              </li>
-              <li>
-                <a href="#a">about</a>
-              </li>
-              <li>
-                <a href="#a">contact</a>
-              </li>
-              <li>
-                <a href="#a">products</a>
-              </li>
+          <div
+            className="links-container show-container"
+            ref={this.linksContainerRef}
+          >
+            <ul className="links" ref={this.linksRef}>
+              {links.map(({ id, url, text }) => {
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
             </ul>
           </div>
           <ul className="social-icons">
-            <li>
-              <a href="https://www.twitter.com">
-                <FaTwitter />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com">
-                <FaTwitter />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com">
-                <FaTwitter />
-              </a>
-            </li>
+            {social.map(({ id, url, icon }) => {
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </nav>
