787bdd7853f579c43484dcf8db07f4a03c0a7ba8
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-01 00:10:45 -0300

    adding button style

--- Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/src/App.js
+++ Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/src/App.js
@@ -26,7 +26,10 @@ class App extends Component {
         <section className="container">
           <h3>{people.length} birthdays today</h3>
           <List people={people} />
-          <button onClick={onClickHandler} className="container__btn">
+          <button
+            onClick={onClickHandler}
+            className="block w-full border-transparent bg-clrPink mx-auto mt-8 mb-0 capitalize text-xl px-0 py-2 tracking-[0.1rem] rounded outline-1 outline outline-[#f28ab2cc]"
+          >
             clear all
           </button>
         </section>
--- Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/tailwind.config.js
+++ Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/tailwind.config.js
@@ -5,12 +5,16 @@ module.exports = {
     extend: {
       colors: {
         brightRed: "hsl(12, 88%, 59%)",
+        clrPink: "#f28ab2",
       },
       boxShadow: {
         lightShadow: "0 5px 15px rgba(0, 0, 0, 0.1)",
       },
       gridTemplateColumns: {
         "auto-1": "auto 1fr",
+      },
+      outline: {
+        outlinePink: "1px solid rgba(242, 138, 178, 1)",
       },
     },
   },
