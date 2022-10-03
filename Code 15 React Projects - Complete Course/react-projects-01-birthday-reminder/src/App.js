6a7a619306f29da45dc74d7be8ae8e5f5f9b5142
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-03 13:47:05 -0300

    feat: making the app.js into function component

--- Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/src/App.js
+++ Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/src/App.js
@@ -1,43 +1,30 @@
-import { Component } from "react";
+import { useState } from "react";
 import List from "./components/List";
 import { birthdays } from "./data/birthdays";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.onClickHandler = this.onClickHandler.bind(this);
-    this.state = {
-      people: birthdays,
-    };
-  }
+const App = () => {
+  const [people, setPeople] = useState(birthdays);
 
-  onClickHandler() {
-    this.setState({
-      people: [],
-    });
-  }
+  const onClickHandler = () => {
+    setPeople([]);
+  };
 
-  render() {
-    const { people } = this.state;
-    const { onClickHandler } = this;
-
-    return (
-      <main className="min-h-screen flex justify-center items-center">
-        <section className="w-[90vw] max-w-fixedWidth bg-[#fff] rounded px-6 py-8 shadow-darkShadow my-20 mx-0">
-          <h3 className="normal-case font-medium mb-6  md:text-[1.75rem]">
-            {people.length} birthdays today
-          </h3>
-          <List people={people} />
-          <button
-            onClick={onClickHandler}
-            className="block w-full border-transparent bg-clrPink mx-auto mt-8 mb-0 capitalize text-xl px-0 py-2 tracking-[0.1rem] rounded outline-1 outline outline-[#f28ab2cc]"
-          >
-            clear all
-          </button>
-        </section>
-      </main>
-    );
-  }
-}
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <section className="w-[90vw] max-w-fixedWidth bg-[#fff] rounded px-6 py-8 shadow-darkShadow my-20 mx-0">
+        <h3 className="normal-case font-medium mb-6  md:text-[1.75rem]">
+          {people.length} birthdays today
+        </h3>
+        <List people={people} />
+        <button
+          onClick={onClickHandler}
+          className="block w-full border-transparent bg-clrPink mx-auto mt-8 mb-0 capitalize text-xl px-0 py-2 tracking-[0.1rem] rounded outline-1 outline outline-[#f28ab2cc]"
+        >
+          clear all
+        </button>
+      </section>
+    </main>
+  );
+};
 
 export default App;
