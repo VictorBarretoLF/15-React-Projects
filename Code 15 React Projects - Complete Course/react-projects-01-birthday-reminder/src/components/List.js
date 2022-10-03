ca2d70d7ecb907648cad30d8e10c90c8a3ab0ca2
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-03 13:48:25 -0300

    finished using hooks into the app

--- Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/src/components/List.js
+++ Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/src/components/List.js
@@ -1,31 +1,26 @@
-import React, { Component } from "react";
-
-class List extends Component {
-  render() {
-    const { people } = this.props;
-    return (
-      <>
-        {people.map(({ id, name, age, image }) => {
-          return (
-            <article
-              key={id}
-              className="person grid grid-cols-auto-1 gap-3 mb-6 items-center "
-            >
-              <img
-                className="w-[75px] h-[75px] object-cover rounded-full shadow-lightShadow"
-                src={image}
-                alt={name}
-              />
-              <div>
-                <h4 className="mb-1 font-bold">{name}</h4>
-                <p className="mb-0">{age} years</p>
-              </div>
-            </article>
-          );
-        })}
-      </>
-    );
-  }
-}
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map(({ id, name, age, image }) => {
+        return (
+          <article
+            key={id}
+            className="person grid grid-cols-auto-1 gap-3 mb-6 items-center "
+          >
+            <img
+              className="w-[75px] h-[75px] object-cover rounded-full shadow-lightShadow"
+              src={image}
+              alt={name}
+            />
+            <div>
+              <h4 className="mb-1 font-bold">{name}</h4>
+              <p className="mb-0">{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
 
 export default List;
