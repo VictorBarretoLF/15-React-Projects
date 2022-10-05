af239ccde9442114e7836ec47f45f430bacad3b9
Víctor Barreto <victorbarretolins@gmail.com>
2022-10-05 09:08:52 -0300

    starting to learning redux toolkit

--- Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/src/components/List.js
+++ Code 15 React Projects - Complete Course/react-projects-01-birthday-reminder/src/components/List.js
@@ -1,24 +1,20 @@
-import React, { Component } from "react";
-
-class List extends Component {
-  render() {
-    const { people } = this.props;
-    return (
-      <>
-        {people.map(({ id, name, age, image }) => {
-          return (
-            <article key={id} className="person">
-              <img src={image} alt={name} />
-              <div>
-                <h4>{name}</h4>
-                <p>{age} years</p>
-              </div>
-            </article>
-          );
-        })}
-      </>
-    );
-  }
-}
+const List = () => {
+  const { people } = this.props;
+  return (
+    <>
+      {people.map(({ id, name, age, image }) => {
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
 
 export default List;
